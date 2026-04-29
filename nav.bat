1174db6b14730f4739f2dada9cba9e8153176021
lastlord44 <36524835+lastlord44@users.noreply.github.com>
2026-04-29 15:42:20 +0300

    chore: remove accidental PR artifacts

--- nav.bat
+++
@@ -1,2 +0,0 @@
-@echo off  
-"C:\Users\musab\AppData\Local\Android\Sdk\platform-tools\adb.exe" -s e3484f25 shell am start -a android.intent.action.VIEW -d "okulakli://(student)/index" com.okulakli.development  
